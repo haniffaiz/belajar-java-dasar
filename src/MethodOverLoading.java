9529d81bfd53f63df8e8ab5a230c0e39cfdf6c11
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-01 13:58:15 +0700

    Tambah MethodOverLoading

---
+++ src/MethodOverLoading.java
@@ -0,0 +1,21 @@
+public class MethodOverLoading {
+    public static void main(String[] args) {
+
+        sayHello();
+        sayHello("Hanif");
+        sayHello("Hanif", "Faiz");
+
+    }
+
+    static void sayHello(){
+        System.out.println("Hello");
+    }
+
+    static void sayHello(String name){
+        System.out.println("Hello " + name);
+    }
+
+    static void sayHello(String firstName, String lastName){
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
